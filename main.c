4cee0313ac47c5d15cc5bc32899c4ea831a02b1d
crrenteria <carlos.renteria@sjsu.edu>
2014-04-19 19:43:01 -0700

    Child 1 reads from stein

--- main.c
+++ main.c
@@ -22,9 +22,10 @@ void write_to_pipe (int file) {
 
 int main (void) {
     /* The five processess */
-    pid_t pid_1, pid_2, pid_3, pid_4, pid_5;
+    pid_t pid;
     /* The five pipes */
     int pipe_1[2], pipe_2[2], pipe_3[2], pipe_4[2], pipe_5[2];
+    
     
     /*Create the pipes*/
     if (pipe(pipe_1)) { //Use this pipe for keyboard input
@@ -46,31 +47,31 @@ int main (void) {
     
     /* Create the child process */
     int nbytes;
-    char string[] = "Child 1 message 2";
+    //char string[] = "Child 1 message 2";
     char readBuffer[80];
-    pid_1 = fork();
-    if (pid_1 == (pid_t) 0) { //Needs a loop to ask for user input.
+    char input[256];
+    //pid = fork();
+    if ((pid = fork()) == (pid_t) 0) { //Needs a loop to ask for user input.
         //This process should terminate when time runs out.
         /* This is the child process. Close other end first */
         close(pipe_1[0]);
         printf("Child 1: Process started.\n");
-        printf("Child 1: Writing to child pipe #1\n");
+        printf("Child 1: Waiting for user input\n");
+        //gets user input write to pipe
+        scanf("%[^\n]s", input);
+        printf("%s\n", input);
+        write(pipe_1[1], input, (strlen(input)+1));
         //write_to_pipe(pipe_1[1]);
-        write(pipe_1[1], string, (strlen(string)+1));
+        //write(pipe_1[1], string, (strlen(string)+1));
         printf("Child 1: Finished writing\n");
         printf("Child 1: Terminating.\n");
-        exit(0);
+        //exit(0);
     }
-    else if (pid_1 < (pid_t) 0) {
-        /* The fork failed */
-        fprintf(stderr, "Fork failed\n");
-        exit(1);
-    }
-    else {
+    else if (pid > (pid_t) 0) {
         /* This is the parent process. Close other end first */
         int status;
         close(pipe_1[1]);
-        printf("Parent: Waiting for input on pipe #1\n");        
+        printf("Parent: Waiting for input on pipe #1\n");
         waitpid(-1, &status, 0);
         //printf("Parent: Reading from child pipe #1.\n");
         //read_from_pipe(pipe_1[0]);
@@ -78,6 +79,11 @@ int main (void) {
         printf("Parent: Read %s from child pipe #1\n", readBuffer);
         printf("Parent: Terminating.\n");
         //exit(0);
+    }
+    else {
+        /* The fork failed */
+        fprintf(stderr, "Fork failed\n");
+        exit(1);
     }
     
     return 0;
