f0ee765489b8eb2272f15ec41add7977d3083468
Brandon Cruz <brandonc91@gmail.com>
2014-04-23 23:21:47 -0700

    new header for time

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <sys/time.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -22,11 +23,13 @@
  }
  */
 
+
 int main () {
     /* The five processess */
     pid_t pid;
     /* The five pipes */
     int pipe_1[2], pipe_2[2], pipe_3[2], pipe_4[2], pipe_5[2];
+
     
     
     /*Create the pipes*/
@@ -57,10 +60,13 @@ int main () {
         printf("Child 1: Process started.\n");
         printf("Child 1: Waiting for user input\n");
         //gets user input write to pipe
+
         while (scanf("%[^\n]*c", input) > 0) {
             //printf("%s\n", input);
+            
             write(pipe_1[1], input, (strlen(input))+1);
         }
+
         //write_to_pipe(pipe_1[1]);
         //write(pipe_1[1], string, (strlen(string)+1));
         printf("Child 1: Terminating.\n");
@@ -71,6 +77,8 @@ int main () {
             /* Child 2 process close its read end of pipe */
             close(pipe_2[0]);
             printf("Child 2: Starting child 2\n");
+            
+            
             char string[] = "Child 2 message 1\n";
             write(pipe_2[1], string, (strlen(string))+1);
             printf("Child 2: Terminating.\n");
