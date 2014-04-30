e32a8979a289e39cca806a75a0c73da9ce31bead
crrenteria <carlos.renteria@sjsu.edu>
2014-04-30 00:14:42 -0700

    Overhaul
    Kinda works

--- main.c
+++ main.c
@@ -2,11 +2,9 @@
 #include <sys/wait.h>
 #include <sys/time.h>
 #include <unistd.h>
-#include <sys/time.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-<<<<<<< HEAD
 int main() {
     pid_t pid;
     int pipe1[2], pipe2[2], pipe3[2], pipe4[2], pipe5[2];
@@ -14,33 +12,6 @@ int main() {
     int seconds = 0;
     int microseconds = 0;
     int i = 1;
-=======
-/* Not used for now
- void read_from_pipe (int file) {
- FILE *stream;
- int c;
- stream = fdopen (file, "r");
- while ((c = fgetc (stream)) != EOF)
- putchar(c);
- fclose (stream);
- }
- 
- void write_to_pipe (int file) {
- FILE *stream;
- stream = fdopen (file, "w");
- fprintf (stream, "Child 1 message 1\n");
- fclose (stream);
- }
- */
-
-
-int main () {
-    /* The five processess */
-    pid_t pid;
-    /* The five pipes */
-    int pipe_1[2], pipe_2[2], pipe_3[2], pipe_4[2], pipe_5[2];
-
->>>>>>> FETCH_HEAD
     
     fd_set pipes;
     FD_ZERO(&pipes);
@@ -64,7 +35,6 @@ int main () {
      pipe(pipe5);
      /* Create Pipes End */
     
-<<<<<<< HEAD
     FD_SET(pipe1[0], &pipes);
     FD_SET(pipe2[0], &pipes);
     FD_SET(pipe3[0], &pipes);
@@ -98,41 +68,6 @@ int main () {
         }
         else {
             pid = fork();
-=======
-    char readBuffer[80];
-    char input[256];
-    //pid = fork();
-    if ((pid = fork()) == 0) { //Needs a loop to ask for user input.
-        //This process should terminate when time runs out.
-        /* Child 1 close the read end of pipe */
-        close(pipe_1[0]);
-        printf("Child 1: Process started.\n");
-        printf("Child 1: Waiting for user input\n");
-        //gets user input write to pipe
-
-        while (scanf("%[^\n]*c", input) > 0) {
-            //printf("%s\n", input);
-            
-            write(pipe_1[1], input, (strlen(input))+1);
-        }
-
-        //write_to_pipe(pipe_1[1]);
-        //write(pipe_1[1], string, (strlen(string)+1));
-        printf("Child 1: Terminating.\n");
-        //exit(0);
-    } else if (pid > 0) {
-        
-        if ((pid = fork()) == 0) {
-            /* Child 2 process close its read end of pipe */
-            close(pipe_2[0]);
-            printf("Child 2: Starting child 2\n");
-            
-            
-            char string[] = "Child 2 message 1\n";
-            write(pipe_2[1], string, (strlen(string))+1);
-            printf("Child 2: Terminating.\n");
-        } else if (pid > 0) {
->>>>>>> FETCH_HEAD
             
             if (pid == 0) {
                 close(pipe2[0]);
@@ -177,6 +112,9 @@ int main () {
                     close(pipe1[1]);
                     close(pipe2[1]);
                     close(pipe3[1]);
+                    open(pipe1[0]);
+                    open(pipe2[0]);
+                    open(pipe3[0]);
                     
                     int rand = random() % 3;
                     sleep(rand);
